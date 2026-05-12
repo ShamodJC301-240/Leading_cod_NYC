fd58e8fa358486264321e907800a0f2aaacff8b9
ShamodJC301-240 <92445690+ShamodJC301-240@users.noreply.github.com>
2026-05-12 18:35:07 -0400

    Formatting clean up

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,57 +1,59 @@
 import pandas as pd
 from pathlib import Path
 
-# Project root directory. Basically clean_data.py.absolute_path().parent()-->src.parent()-->New_York_Leading_Cause_Of_Death
+# Project root
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Our data paths
+# Data path
 
-raw_data_path = (BASE_DIR/ "Data"/ "raw"/ "NY_leading_cause_of_death.csv")
+RAW_DATA_PATH = BASE_DIR / "Data" / "raw" / "NY_leading_cause_of_death.csv"
+PROCESSED_DATA_DIR = BASE_DIR / "Data" / "processed"
 
-processed_data_dir = (BASE_DIR/ "Data"/ "processed")
+# Create processed folder if it doesn't exist
+PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-# creates processed folder if it doesnt exist
-processed_data_dir.mkdir(parents=True,exist_ok=True)
+# Load raw data
 
-# Load raw dataset
+leading_cause_nyc = pd.read_csv(RAW_DATA_PATH)
 
-leading_cause_nyc = pd.read_csv(raw_data_path)
+print("\nRaw dataset loaded successfully.")
 
-# Basic data inspection
-
+# Basic inspection
+ 
 print("\nFirst five rows:")
 print(leading_cause_nyc.head())
 
-print("\nDataset shape:")
-print(leading_cause_nyc.shape)
+rows, cols = leading_cause_nyc.shape
+print(f"\nDataset shape → {rows} rows, {cols} columns")
 
-# Duplicate check
+# Duplicates
+ 
+num_duplicates = leading_cause_nyc.duplicated().sum()
+print(f"\nNumber of duplicates: {num_duplicates}")
 
-num_of_duplicates = (leading_cause_nyc.duplicated().sum())
+leading_cause_nyc = leading_cause_nyc.drop_duplicates()
 
-print(f"\nNumber of duplicates: {num_of_duplicates}")
+rows, cols = leading_cause_nyc.shape
+print(f"\nShape after removing duplicates → {rows} rows, {cols} columns")
 
-# Drop duplicates
-
-leading_cause_nyc = (leading_cause_nyc.drop_duplicates())
-
-print("\nShape after duplicates removed:")
-print(leading_cause_nyc.shape)
-
-# Clean cloumn names
+# Column cleaning
 
 leading_cause_nyc.columns = (leading_cause_nyc.columns.str.strip().str.lower().str.replace(" ", "_"))
+cleaned_columns = leading_cause_nyc.columns
 
 print("\nCleaned column names:")
-print(leading_cause_nyc.columns)
+for col in cleaned_columns:
+    print(f'- {col}')
 
-# Save clean dataset
 
-output_path = (processed_data_dir/ "NY_leading_cause_of_death_cleaned.csv")
 
-leading_cause_nyc.to_csv(output_path,index=False)
+# Save cleaned data to processed csv
+
+output_path = PROCESSED_DATA_DIR / "NY_leading_cause_of_death_cleaned.csv"
+
+leading_cause_nyc.to_csv(output_path, index=False)
 
 print("\nCleaned dataset saved successfully.")
 
-print(f"\nSaved to:\n{output_path}")
+print(f"Saved to:\n{output_path}")
