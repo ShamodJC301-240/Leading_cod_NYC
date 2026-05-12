90695f02dbd0d933489426e21769ab37f56ee577
ShamodJC301-240 <92445690+ShamodJC301-240@users.noreply.github.com>
2026-05-12 13:37:43 -0400

    Cleaning up relative paths

--- src/api_pull.py
+++ src/api_pull.py
@@ -1,12 +1,22 @@
 import requests
 import pandas as pd
+from pathlib import Path
 
+# Project root directory. Set for resolving relative path issues
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# NYC OPEN DATA API ENDPOINT
+# Output directory
+
+RAW_DATA_DIR = BASE_DIR / "Data" / "raw"
+
+# create raw folder if it doesnt exist. Removes ambiguity.
+RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+# NYC Open Data API endpoint
 
 url = "https://data.cityofnewyork.us/resource/jb7j-dtam.json"
 
-# PAGINATION SETUP
+# Pagination loop for limit handling
 
 limit = 1000
 offset = 0
@@ -15,8 +25,7 @@ all_data = []
 
 print("Pulling data from API...\n")
 
-
-# PAGINATION LOOP
+# Pagination loop
 
 while True:
 
@@ -27,32 +36,32 @@ while True:
 
     response = requests.get(url, params=params)
 
+    # convert response into json
     data = response.json()
 
     # stopping condition
     if not data:
         break
 
-    # appending rows into master list
+    # append rows into master list
     all_data.extend(data)
 
     print(f"Pulled {len(data)} rows | Total: {len(all_data)}")
 
-    # moving offset window
+    # move pagination window
     offset += limit
 
-
-# CREATING DATAFRAME
+# Creating our dataframe
 
 leading_cause_nyc = pd.DataFrame(all_data)
 
+# Save raw data to csv
 
-# SAVING RAW DATA
+output_path = (RAW_DATA_DIR/ "NY_leading_cause_of_death.csv")
 
-leading_cause_nyc.to_csv(
-    "../Data/raw/NY_leading_cause_of_death.csv",
-    index=False
-)
+leading_cause_nyc.to_csv(output_path,index=False)
 
 print("\nDataset saved successfully.")
 print(leading_cause_nyc.shape)
+
+print(f"\nSaved to:\n{output_path}")
--- src/clean_data.py
+++ src/clean_data.py
@@ -1,13 +1,24 @@
 import pandas as pd
+from pathlib import Path
 
+# Project root directory. Basically clean_data.py.absolute_path().parent(src).parent(New_York_Leading_Cause_Of_Death)
 
-# LOADING RAW DATASET
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-leading_cause_nyc = pd.read_csv(
-    "../Data/raw/NY_leading_cause_of_death.csv"
-)
+# Our data paths
 
-# BASIC DATA INSPECTION
+raw_data_path = (BASE_DIR/ "Data"/ "raw"/ "NY_leading_cause_of_death.csv")
+
+processed_data_dir = (BASE_DIR/ "Data"/ "processed")
+
+# creates processed folder if it doesnt exist
+processed_data_dir.mkdir(parents=True,exist_ok=True)
+
+# Load raw dataset
+
+leading_cause_nyc = pd.read_csv(raw_data_path)
+
+# Basic data inspection
 
 print("\nFirst five rows:")
 print(leading_cause_nyc.head())
@@ -15,39 +26,32 @@ print(leading_cause_nyc.head())
 print("\nDataset shape:")
 print(leading_cause_nyc.shape)
 
+# Duplicate check
 
-# DUPLICATE CHECK
-
-num_of_duplicates = leading_cause_nyc.duplicated().sum()
+num_of_duplicates = (leading_cause_nyc.duplicated().sum())
 
 print(f"\nNumber of duplicates: {num_of_duplicates}")
 
-# DROPPING DUPLICATES
+# Drop duplicates
 
-leading_cause_nyc = leading_cause_nyc.drop_duplicates()
+leading_cause_nyc = (leading_cause_nyc.drop_duplicates())
 
 print("\nShape after duplicates removed:")
 print(leading_cause_nyc.shape)
 
+# Clean cloumn names
 
-# CLEANING COLUMN NAMES
-
-leading_cause_nyc.columns = (
-    leading_cause_nyc.columns
-    .str.strip()
-    .str.lower()
-    .str.replace(" ", "_")
-)
+leading_cause_nyc.columns = (leading_cause_nyc.columns.str.strip().str.lower().str.replace(" ", "_"))
 
 print("\nCleaned column names:")
 print(leading_cause_nyc.columns)
 
+# Save clean dataset
 
-# SAVING CLEANED DATASET
+output_path = (processed_data_dir/ "NY_leading_cause_of_death_cleaned.csv")
 
-leading_cause_nyc.to_csv(
-    "../Data/processed/NY_leading_cause_of_death_cleaned.csv",
-    index=False
-)
+leading_cause_nyc.to_csv(output_path,index=False)
 
 print("\nCleaned dataset saved successfully.")
+
+print(f"\nSaved to:\n{output_path}")
